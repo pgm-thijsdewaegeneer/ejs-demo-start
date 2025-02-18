725edfd00ff5b463ee4004e739abbf2e98e6ed90
Frederick Roegiers <frederick@rogerthat.be>
2025-02-18 11:29:57 +0100

    wip

--- src/controllers/PageController.js
+++ src/controllers/PageController.js
@@ -1,15 +1,20 @@
 /**
  *
- * todo 1: render de person
+ * todo 1: render de person in contact
+ * todo 2: render de navigatie items in ELKE template
+ *  STAP 1: importeer
+ *  STAP 2: voeg toe aan het renderobject
+ *  STAP 3: voeg een navigatiebalk toe aan alle templates
  */
 
-import { person } from "../data/data.js";
+import { person, navItems } from "../data/data.js";
 
 export const home = (req, res) => {
   // res.render("<NAAM VAN DE VIEW>", {<DATA NAAR DE VIEW>})
   res.render("pages/home", {
     title: "Dinosaurs around the world",
     content: "Welcome to the world of dinosaurs",
+    navItems,
   });
 };
 export const about = (req, res) => {
@@ -22,6 +27,7 @@ export const about = (req, res) => {
       "Stegosaurus",
       "<strong>Premium</strong> Diplodocus",
     ],
+    navItems,
   });
 };
 export const contact = (req, res) => {
@@ -29,6 +35,7 @@ export const contact = (req, res) => {
     title: "Contact",
     content: "Contact us at 09 233 40 89",
     person,
+    navItems,
   });
 };
 export const privacy = (req, res) => {
@@ -36,5 +43,6 @@ export const privacy = (req, res) => {
     title: "Privacy Policy",
     content: "Dinosaurs are protective of their privacy",
     team: false,
+    navItems,
   });
 };
